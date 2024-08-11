5c9b8284976a64b589ac5a4a63e70fa07bb8f935
Neiland85 <bookingnadarecords@gmail.com>
2024-08-11 20:31:12 +0200

    Initial commit - Backend setup

---
+++ src/server.js
@@ -0,0 +1,4 @@
+MONGODB_URI=mongodb+srv://<bookingnadarecrds@gmail.com>:<LTltqZFVhxeCte3b>@cluster0.mongodb.net/FlirtGenius?retryWrites=true&w=majority
+STRIPE_SECRET_KEY=tu_clave_secreta_de_stripe
+FRONTEND_URL=http://localhost:0000
+
